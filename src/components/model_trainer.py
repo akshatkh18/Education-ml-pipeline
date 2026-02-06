01f0cfac0fc2785e6a1d6de7457640258c034ae5
Akshat Gupta <175546424+akshatkh18@users.noreply.github.com>
2026-02-06 21:50:50 +0530

    Update ModelTraining.py

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -22,10 +22,13 @@ from src.utils import save_object, evaluate_model
 @dataclass
 class ModelTrainerConfig:
     trained_model_file_path=os.path.join("artifacts", "model.pkl")
+    catboost_log_dir = os.path.join("artifacts", "catboost_logs")
 
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
+        os.makedirs(self.model_trainer_config.catboost_log_dir, exist_ok=True)
+
 
     def initiate_model_trainer(self, train_arr,test_arr):
             try:
@@ -44,14 +47,65 @@ class ModelTrainer:
                     "K-Neighbors Regressor": KNeighborsRegressor(),
                     "XGBRegressor": XGBRegressor(),
                     "Random Forest Regressor": RandomForestRegressor(),
-                    "CatBoosting Regressor": CatBoostRegressor(verbose=False),
-                    "AdaBoost Regressor": AdaBoostRegressor()
-
+                    "CatBoosting Regressor": CatBoostRegressor(verbose=False, train_dir=self.model_trainer_config.catboost_log_dir),
+                    "AdaBoost Regressor": AdaBoostRegressor(),
+                    "Decision Tree":DecisionTreeRegressor(),
+                    "Gradient Boosting":GradientBoostingRegressor()
                 }
-                model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+
+                params={
+                    "Decision Tree":{
+                        'criterion':['squared_error', 'absolute_error', 'friedman_mse', 'poisson'],
+                        'splitter':['best', 'random'],
+                        'max_features':['sqrt', 'log2'],
+                    },
+                    "Random Forest Regressor":{
+                        'criterion':['squared_error', 'absolute_error', 'friedman_mse', 'poisson'],
+                        'n_estimators':[8, 16, 32, 64, 128, 256],
+                        'max_features':['sqrt', 'log2'],
+
+                    },
+
+                    "Gradient Boosting":{
+                        "n_estimators":[8, 16, 32, 64, 128, 256],
+                        "learning_rate":[0.1,0.01,0.001,0.0001],
+                        'subsample':[0.6,0.7,0.75,0.8,0.8,0.85,0.9],
+                        'loss':['squared_error', 'absolute_error', 'huber', 'quantile'],
+                        'max_features':['sqrt', 'log2'],
+                        'criterion':['squared_error', 'friedman_mse'],
+                    },
+
+                    "Linear Regression":{},
+                    "Lasso": {},
+                    "Ridge": {},
+
+                    "K-Neighbors Regressor":{
+                        "n_neighbors":[5,7,9,11],
+                        "weights":['uniform','distance'],
+                        'algorithm':['ball_tree', 'kd_tree','brute']
+                    },
+
+                    "XGBRegressor":{
+                        "learning_rate":[0.1,0.01,0.001,0.0001],
+                        "n_estimators":[8, 16, 32, 64, 128, 256]
+                    },
+                    "CatBoosting Regressor":{
+                        'depth':[6,8,10],
+                        "learning_rate":[0.1,0.01,0.001,0.0001],
+                        'iterations':[30,50,100],
+                    },
+
+                    "AdaBoost Regressor":{
+                        'learning_rate':[0.1,0.01,0.001,0.0001],
+                        'loss':['linear','square','exponential'],
+                        "n_estimators":[8, 16, 32, 64, 128, 256]
+                    }
+                }
+                
+                model_report,best_models=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models, param=params)
 
                 best_model_name = max(model_report, key=model_report.get)
-                best_model = models[best_model_name]
+                best_model = best_models[best_model_name]
                 best_model_score = model_report[best_model_name]
 
                 if best_model_score<0.6:
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import dill
 import pandas as pd 
 import numpy as np
 
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import r2_score
 
 from src.exception import CustomException
@@ -22,21 +23,32 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_model(X_train, y_train,X_test,y_test,models):
+def evaluate_model(X_train, y_train,X_test,y_test,models,param):
     try:
         report={}
-        for name, model in models.items():
-            model.fit(X_train, y_train)
+        best_models={}
+        for name, model in models.items():            
+            para = param.get(name, {})
 
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
+            if para:
+                gs = GridSearchCV(model,para,cv=3,scoring='r2',n_jobs=-1)              
+                gs.fit(X_train, y_train)
+                best_model=gs.best_estimator_                
+
+            else:
+                model.fit(X_train, y_train)
+                best_model = model
+
+            y_train_pred = best_model.predict(X_train)
+            y_test_pred = best_model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
-            test_model_score = r2_score(y_test, y_test_pred)
+            test_model_score = r2_score(y_test, y_test_pred)           
 
             report[name] = test_model_score
+            best_models[name] = best_model
 
-        return report
+        return report,best_models
 
     except Exception as e:
         raise CustomException(e, sys) 
